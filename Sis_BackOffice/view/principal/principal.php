f9c0a5292fa24ff60c6919a0d71a2ca60f3b3148
diario <ahnimargot@gmail.com>
2024-11-25 07:02:12 -0400

    Login principal controller

--- Sis_BackOffice/view/principal/principal.php
+++ Sis_BackOffice/view/principal/principal.php
@@ -16,7 +16,7 @@
 
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-6">
-		<h1 class="h3 mb-0 text-gray-800">Neuronas</h1>
+		<h1 class="h3 mb-0 text-gray-800">PEDIDOS</h1>
 		<a href="?c=principal&a=Nuevo" class="btn btn-primary">Nuevo</a>
 	</div>
     
